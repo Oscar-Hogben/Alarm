6d194f1c321e6c1efa5eb0004895b38348c91c45
Oscar Hogben <83372155+Oscar-Hogben@users.noreply.github.com>
2021-06-24 09:03:56 +0000

    First Commit

---
+++ WeatherPicker.py
@@ -0,0 +1,13 @@
+class weatherPicker:
+  def Sunny_Warm():
+    return "Its sunny and warm!"
+  def Rainy():
+    return "Its rainy"
+  def Sunny_Cold():
+    return "Its sunny but cold :("
+  def Freezing():
+    return "Its pretty freezing!"
+  def Snowing():
+    return "Its snowing!"
+  def TunderStorm():
+    return "Tunder!"
---
+++ main.py
@@ -0,0 +1,5 @@
+import requests, json, random, playsound
+from WeatherPicker import weatherPicker
+
+response = weatherPicker.Freezing()
+print(response)
